9b0ff473253a34021fb55500a6564b90ec3349bb
Mario Domenech Goulart <mario.goulart@gmail.com>
2016-11-18 21:50:14 +0100

    commands/dump: add -s option to dump in sexp format

--- src/commands/dump.scm
+++ src/commands/dump.scm
@@ -1,9 +1,16 @@
 (define help-dump
   #<#EOF
-dump
-  Dump the database content.
+dump [-s]
+  Dump the database content.  If -s is provided, dump in sexp format.
 EOF
 )
 
-(define (cmd-dump)
-  (for-each print-vault-obj (db-dump-objects)))
+(define (cmd-dump args)
+  (let ((sexp-format? (and (not (null? args))
+                           (string=? (car args) "-s"))))
+    (for-each
+     (lambda (obj)
+       (if sexp-format?
+           (pp (vault-obj->alist obj))
+           (print-vault-obj obj)))
+     (db-dump-objects))))
