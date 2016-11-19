150b41993bc36cb3652c4e22f0f504c9af4e8f72
Mario Domenech Goulart <mario.goulart@gmail.com>
2016-11-19 23:22:43 +0100

    commands/list: add -t and -T options to list by tags

--- src/commands/list.scm
+++ src/commands/list.scm
@@ -1,18 +1,46 @@
 (define help-list
   #<#EOF
-list [-s]
-  List the database content.  If -s is provided, list in sexp format.
+list [-s] [-t <tag>] [-T <tag>]
+  List the database content.
+  -s: list in sexp format.
+  -t: list entries that contain <tag>.  May be provided multiple times.
+  -T: do not list entries that contain <tag>.  May be provided multiple times.
 EOF
 )
 
 (define (cmd-list args)
-  (let ((sexp-format? (and (not (null? args))
-                           (string=? (car args) "-s"))))
-    (with-output-to-pager
-     (lambda ()
-       (for-each
-        (lambda (obj)
-          (if sexp-format?
-              (pp (vault-obj->alist obj))
-              (print-vault-obj obj)))
-        (db-dump-objects))))))
+  (let ((list-tags '())
+        (omit-tags '())
+        (sexp-format? #f))
+    (let loop ((args args))
+      (unless (null? args)
+        (let ((arg (car args)))
+          (cond ((string=? arg "-t")
+                 (set! list-tags (cons (parse-option/arg args "-t") list-tags))
+                 (loop (cddr args)))
+                ((string=? arg "-T")
+                 (set! omit-tags (cons (parse-option/arg args "-T") omit-tags))
+                 (loop (cddr args)))
+                ((string=? arg "-s")
+                 (set! sexp-format? #t)
+                 (loop (cdr args)))
+                (else (die! "list: invalid syntax."))))))
+    (let ((objs (remove (lambda (obj)
+                          (any (lambda (tag)
+                                 (member tag omit-tags))
+                               (vault-obj-tags obj)))
+                        (if (null? list-tags)
+                            (db-dump-objects)
+                            (filter (lambda (obj)
+                                      (any (lambda (tag)
+                                             (member tag list-tags))
+                                           (vault-obj-tags obj)))
+                                    (db-dump-objects))))))
+      (with-output-to-pager
+       (lambda ()
+         (for-each
+          (lambda (obj)
+            (if sexp-format?
+                (pp (vault-obj->alist obj))
+                (print-vault-obj obj)))
+          objs))))))
