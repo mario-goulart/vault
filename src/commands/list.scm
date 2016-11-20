f420f667873f2aa201b953ba5e495e2a74c07dcb
Mario Domenech Goulart <mario.goulart@gmail.com>
2016-11-20 13:30:18 +0100

    commands/list: add -i option to list specific object by id

--- src/commands/list.scm
+++ src/commands/list.scm
@@ -1,16 +1,18 @@
 (define help-list
   #<#EOF
-list [-s] [-t <tag>] [-T <tag>]
+list [-s] [-t <tag>] [-T <tag>] [-i <id>]
   List the database content.
   -s: list in sexp format.
   -t: list entries that contain <tag>.  May be provided multiple times.
   -T: do not list entries that contain <tag>.  May be provided multiple times.
+  -i: list <id>, ignoring tag filters.  May be provided multiple times.
 EOF
 )
 
 (define (cmd-list args)
   (let ((list-tags '())
         (omit-tags '())
+        (ids '())
         (sexp-format? #f))
     (let loop ((args args))
       (unless (null? args)
@@ -21,21 +23,27 @@ EOF
                 ((string=? arg "-T")
                  (set! omit-tags (cons (parse-option/arg args "-T") omit-tags))
                  (loop (cddr args)))
+                ((string=? arg "-i")
+                 (set! ids (cons (parse-option/arg args "-i") ids))
+                 (loop (cddr args)))
                 ((string=? arg "-s")
                  (set! sexp-format? #t)
                  (loop (cdr args)))
                 (else (die! "list: invalid syntax."))))))
-    (let ((objs (remove (lambda (obj)
-                          (any (lambda (tag)
-                                 (member tag omit-tags))
-                               (vault-obj-tags obj)))
-                        (if (null? list-tags)
-                            (db-dump-objects)
-                            (filter (lambda (obj)
-                                      (any (lambda (tag)
-                                             (member tag list-tags))
-                                           (vault-obj-tags obj)))
-                                    (db-dump-objects))))))
+    (let ((objs (if (null? ids)
+                    (remove (lambda (obj)
+                              (any (lambda (tag)
+                                     (member tag omit-tags))
+                                   (vault-obj-tags obj)))
+                            (if (null? list-tags)
+                                (db-dump-objects)
+                                (filter (lambda (obj)
+                                          (any (lambda (tag)
+                                                 (member tag list-tags))
+                                               (vault-obj-tags obj)))
+                                        (db-dump-objects))))
+                    (filter-map db-get-vault-object-by-id
+                                (sort (filter-map string->number ids) <)))))
       (with-output-to-pager
        (lambda ()
          (let loop ((objs objs))
